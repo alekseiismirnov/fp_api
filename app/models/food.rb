e58d9b9584022e59d23847cfe1b07e8a63299c3c
Alekseii Sm <alekseii.sm@gmail.com>
2024-11-05 19:56:43 +0200

    add food model, validators, and tests

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
 class Food < ApplicationRecord
   validates :name, :proteins, :carbs, :fats, presence: true
   validates :name, uniqueness: true
+  validate  :sum_of_nutriens_not_greater_100, if: :all_nutrients_present?
+
+  private
+
+  def all_nutrients_present?
+    [proteins, carbs, fats].none? nil
+  end
+
+  def sum_of_nutriens_not_greater_100
+    return unless (proteins || (0 + carbs) || (0 + fats) || 0) > 100
+
+    errors.add 'Sum of nutriens can not be more then 100'
+  end
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Food, type: :model do
+RSpec.describe Food do
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :proteins }
   it { is_expected.to validate_presence_of :fats }
